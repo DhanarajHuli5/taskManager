39e35addec1df08f328db79079725a7f7e0aade6
DhanarajHuli5 <dhanarajhuli5@gmail.com>
2025-06-10 01:57:28 +0530

    feat: implement user registration and validation logic

---
+++ src/validators/index.js
@@ -0,0 +1,37 @@
+import {body} from "express-validator"
+
+const userRegistrationValidator = () => {
+    return [
+        body('email')
+        .trim()
+        .notEmpty().withMessage("Email is required")
+        .isEmail().withMessage("Email is not valid"),
+        body('username')
+        .trim()
+        .notEmpty().withMessage("Username is required")
+        .isLength({min: 3}).withMessage("Username must be at least 3 characters long")
+        .isLength({max: 13}).withMessage("Username must be at most 13 characters long")
+        .matches(/^[a-zA-Z0-9_]+$/).withMessage("Username must contain only letters, numbers, and underscores"),
+        body('password')
+        .trim()
+        .notEmpty().withMessage("Password is required")
+        .isLength({min: 8}).withMessage("Password must be at least 8 characters long"),
+        body('role')
+        .trim()
+        .notEmpty().withMessage("Role is required")
+    ]
+}
+
+const userLoginValidator = () => {
+    return [
+        body('email')
+        .trim()
+        .notEmpty().withMessage("Email is required")
+        .isEmail().withMessage("Email is not valid"),
+        body('password')
+        .trim()
+        .notEmpty().withMessage("Password is required")
+    ]
+}
+
+export {userRegistrationValidator, userLoginValidator}
